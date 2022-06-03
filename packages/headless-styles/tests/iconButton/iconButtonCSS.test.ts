bfd4886d41e3f21790dc1cd8c5d9af6071b26bc5
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-06-03 11:21:41 -0400

    refactor(headless-styles): remove needless describes blocks

--- packages/headless-styles/tests/iconButton/iconButtonCSS.test.ts
+++ packages/headless-styles/tests/iconButton/iconButtonCSS.test.ts
@@ -1,121 +1,117 @@
 import { getIconButtonProps } from '../../src'
 
-describe('Icon Button CSS', () => {
-  describe('getIconButtonProps', () => {
-    const baseClass = 'ps-icon-btn'
-    const result = {
+describe('Icon Button CSS (getIconButtonProps)', () => {
+  const baseClass = 'ps-icon-btn'
+  const result = {
+    button: {
+      'aria-label': 'button',
+      className: `ps-btn text m ${baseClass} mIconButton defaultIconButton`,
+      type: 'button',
+    },
+    iconOptions: {
+      size: 'm',
+      ariaHidden: 'true',
+    },
+  }
+
+  test('should accept an ariaLabel argument', () => {
+    expect(getIconButtonProps({ ariaLabel: 'button' })).toEqual(result)
+  })
+
+  test('should accept a kind type', () => {
+    expect(getIconButtonProps({ ariaLabel: 'button', kind: 'text' })).toEqual(
+      result
+    )
+    expect(
+      getIconButtonProps({ ariaLabel: 'button', kind: 'textWeak' })
+    ).toEqual({
       button: {
-        'aria-label': 'button',
-        className: `ps-btn text m ${baseClass} mIconButton defaultIconButton`,
-        type: 'button',
+        ...result.button,
+        className: `ps-btn textWeak m ${baseClass} mIconButton defaultIconButton`,
       },
-      iconOptions: {
-        size: 'm',
-        ariaHidden: 'true',
-      },
-    }
-
-    test('should accept an ariaLabel argument', () => {
-      expect(getIconButtonProps({ ariaLabel: 'button' })).toEqual(result)
+      iconOptions: result.iconOptions,
     })
-
-    test('should accept a kind type', () => {
-      expect(getIconButtonProps({ ariaLabel: 'button', kind: 'text' })).toEqual(
-        result
-      )
-      expect(
-        getIconButtonProps({ ariaLabel: 'button', kind: 'textWeak' })
-      ).toEqual({
-        button: {
-          ...result.button,
-          className: `ps-btn textWeak m ${baseClass} mIconButton defaultIconButton`,
-        },
-        iconOptions: result.iconOptions,
-      })
-      expect(getIconButtonProps({ ariaLabel: 'button', kind: 'weak' })).toEqual(
-        {
-          button: {
-            ...result.button,
-            className: `ps-btn weak m ${baseClass} mIconButton defaultIconButton`,
-          },
-          iconOptions: result.iconOptions,
-        }
-      )
-      expect(
-        getIconButtonProps({ ariaLabel: 'button', kind: 'medium' })
-      ).toEqual({
+    expect(getIconButtonProps({ ariaLabel: 'button', kind: 'weak' })).toEqual({
+      button: {
+        ...result.button,
+        className: `ps-btn weak m ${baseClass} mIconButton defaultIconButton`,
+      },
+      iconOptions: result.iconOptions,
+    })
+    expect(getIconButtonProps({ ariaLabel: 'button', kind: 'medium' })).toEqual(
+      {
         button: {
           ...result.button,
           className: `ps-btn medium m ${baseClass} mIconButton defaultIconButton`,
         },
         iconOptions: result.iconOptions,
-      })
-      expect(
-        getIconButtonProps({ ariaLabel: 'button', kind: 'strong' })
-      ).toEqual({
+      }
+    )
+    expect(getIconButtonProps({ ariaLabel: 'button', kind: 'strong' })).toEqual(
+      {
         button: {
           ...result.button,
           className: `ps-btn strong m ${baseClass} mIconButton defaultIconButton`,
         },
         iconOptions: result.iconOptions,
-      })
-    })
+      }
+    )
+  })
 
-    test('should accept a size type', () => {
-      expect(getIconButtonProps({ ariaLabel: 'button', size: 'm' })).toEqual(
-        result
-      )
-      expect(getIconButtonProps({ ariaLabel: 'button', size: 'xs' })).toEqual({
-        button: {
-          ...result.button,
-          className: `ps-btn text xs ${baseClass} xsIconButton defaultIconButton`,
-        },
-        iconOptions: {
-          ...result.iconOptions,
-          size: 's',
-        },
-      })
-      expect(getIconButtonProps({ ariaLabel: 'button', size: 's' })).toEqual({
-        button: {
-          ...result.button,
-          className: `ps-btn text s ${baseClass} sIconButton defaultIconButton`,
-        },
-        iconOptions: result.iconOptions,
-      })
-      expect(getIconButtonProps({ ariaLabel: 'button', size: 'l' })).toEqual({
-        button: {
-          ...result.button,
-          className: `ps-btn text l ${baseClass} lIconButton defaultIconButton`,
-        },
-        iconOptions: {
-          ...result.iconOptions,
-          size: 'l',
-        },
-      })
+  test('should accept a size type', () => {
+    expect(getIconButtonProps({ ariaLabel: 'button', size: 'm' })).toEqual(
+      result
+    )
+    expect(getIconButtonProps({ ariaLabel: 'button', size: 'xs' })).toEqual({
+      button: {
+        ...result.button,
+        className: `ps-btn text xs ${baseClass} xsIconButton defaultIconButton`,
+      },
+      iconOptions: {
+        ...result.iconOptions,
+        size: 's',
+      },
     })
+    expect(getIconButtonProps({ ariaLabel: 'button', size: 's' })).toEqual({
+      button: {
+        ...result.button,
+        className: `ps-btn text s ${baseClass} sIconButton defaultIconButton`,
+      },
+      iconOptions: result.iconOptions,
+    })
+    expect(getIconButtonProps({ ariaLabel: 'button', size: 'l' })).toEqual({
+      button: {
+        ...result.button,
+        className: `ps-btn text l ${baseClass} lIconButton defaultIconButton`,
+      },
+      iconOptions: {
+        ...result.iconOptions,
+        size: 'l',
+      },
+    })
+  })
 
-    test('should accept a tech type', () => {
-      expect(
-        getIconButtonProps({ ariaLabel: 'button', tech: 'svelte' })
-      ).toEqual({
+  test('should accept a tech type', () => {
+    expect(getIconButtonProps({ ariaLabel: 'button', tech: 'svelte' })).toEqual(
+      {
         button: {
           'aria-label': 'button',
           class: 'base text m baseIconButton mIconButton defaultIconButton',
         },
         iconOptions: result.iconOptions,
-      })
-    })
+      }
+    )
+  })
 
-    test('should accept a variant', () => {
-      expect(
-        getIconButtonProps({ ariaLabel: 'button', variant: 'round' })
-      ).toEqual({
-        button: {
-          ...result.button,
-          className: `ps-btn text m ${baseClass} mIconButton roundIconButton`,
-        },
-        iconOptions: result.iconOptions,
-      })
+  test('should accept a variant', () => {
+    expect(
+      getIconButtonProps({ ariaLabel: 'button', variant: 'round' })
+    ).toEqual({
+      button: {
+        ...result.button,
+        className: `ps-btn text m ${baseClass} mIconButton roundIconButton`,
+      },
+      iconOptions: result.iconOptions,
     })
   })
 })
--- packages/headless-styles/tests/iconButton/iconButtonJS.test.ts
+++ packages/headless-styles/tests/iconButton/iconButtonJS.test.ts
@@ -1,96 +1,94 @@
 import { getJSIconButtonProps } from '../../src'
 
-describe('Icon Button JS', () => {
-  describe('getJSIconButtonProps', () => {
-    test('should accept an ariaLabel option', () => {
-      const { button } = getJSIconButtonProps({ ariaLabel: 'icon button' })
+describe('Icon Button JS (getJSIconButtonProps)', () => {
+  test('should accept an ariaLabel option', () => {
+    const { button } = getJSIconButtonProps({ ariaLabel: 'icon button' })
 
-      expect(button.cssProps).not.toHaveLength(0)
-      expect(button.styles).toBeDefined()
-      expect(button.type).toEqual('button')
-      expect(button.a11yProps).toEqual({
-        'aria-label': 'icon button',
-      })
+    expect(button.cssProps).not.toHaveLength(0)
+    expect(button.styles).toBeDefined()
+    expect(button.type).toEqual('button')
+    expect(button.a11yProps).toEqual({
+      'aria-label': 'icon button',
+    })
+  })
+
+  test('should return icon options', () => {
+    const { iconOptions } = getJSIconButtonProps({ ariaLabel: 'icon button' })
+
+    expect(iconOptions).toEqual({
+      size: 'm',
+      ariaHidden: 'true',
+    })
+  })
+
+  test('should accept a "round" variant type', () => {
+    const { button } = getJSIconButtonProps({
+      ariaLabel: 'icon button',
+      variant: 'round',
     })
 
-    test('should return icon options', () => {
-      const { iconOptions } = getJSIconButtonProps({ ariaLabel: 'icon button' })
+    expect(button.cssProps).toContain('border-radius: 50%')
+    expect(button.styles.borderRadius).toEqual('50%')
+  })
 
-      expect(iconOptions).toEqual({
-        size: 'm',
-        ariaHidden: 'true',
-      })
+  test('should allow no variant type to be passed in', () => {
+    const { button } = getJSIconButtonProps({
+      ariaLabel: 'icon button',
     })
 
-    test('should accept a "round" variant type', () => {
-      const { button } = getJSIconButtonProps({
-        ariaLabel: 'icon button',
-        variant: 'round',
-      })
+    expect(button.cssProps).toContain('border-radius: 6px')
+    expect(button.styles.borderRadius).toEqual('6px')
+  })
 
-      expect(button.cssProps).toContain('border-radius: 50%')
-      expect(button.styles.borderRadius).toEqual('50%')
+  test('should accept an xs size type', () => {
+    const { button } = getJSIconButtonProps({
+      ariaLabel: 'icon button',
+      size: 'xs',
     })
 
-    test('should allow no variant type to be passed in', () => {
-      const { button } = getJSIconButtonProps({
-        ariaLabel: 'icon button',
-      })
+    expect(button.cssProps).toContain('width: 1.5rem')
+    expect(button.styles.width).toEqual('1.5rem')
 
-      expect(button.cssProps).toContain('border-radius: 6px')
-      expect(button.styles.borderRadius).toEqual('6px')
+    expect(button.cssProps).toContain('padding-inline: 0')
+    expect(button.styles.paddingInline).toEqual('0')
+  })
+
+  test('should accept a small size type', () => {
+    const { button } = getJSIconButtonProps({
+      ariaLabel: 'icon button',
+      size: 's',
     })
 
-    test('should accept an xs size type', () => {
-      const { button } = getJSIconButtonProps({
-        ariaLabel: 'icon button',
-        size: 'xs',
-      })
+    expect(button.cssProps).toContain('width: 2rem')
+    expect(button.styles.width).toEqual('2rem')
 
-      expect(button.cssProps).toContain('width: 1.5rem')
-      expect(button.styles.width).toEqual('1.5rem')
+    expect(button.cssProps).toContain('padding-inline: 0')
+    expect(button.styles.paddingInline).toEqual('0')
+  })
 
-      expect(button.cssProps).toContain('padding-inline: 0')
-      expect(button.styles.paddingInline).toEqual('0')
+  test('should accept a medium size type', () => {
+    const { button } = getJSIconButtonProps({
+      ariaLabel: 'icon button',
+      size: 'm',
     })
 
-    test('should accept a small size type', () => {
-      const { button } = getJSIconButtonProps({
-        ariaLabel: 'icon button',
-        size: 's',
-      })
+    expect(button.cssProps).toContain('width: 2.5rem')
+    expect(button.styles.width).toEqual('2.5rem')
 
-      expect(button.cssProps).toContain('width: 2rem')
-      expect(button.styles.width).toEqual('2rem')
+    expect(button.cssProps).toContain('padding-inline: 0')
+    expect(button.styles.paddingInline).toEqual('0')
+  })
 
-      expect(button.cssProps).toContain('padding-inline: 0')
-      expect(button.styles.paddingInline).toEqual('0')
+  test('should accept a large size type', () => {
+    const { button } = getJSIconButtonProps({
+      ariaLabel: 'icon button',
+      size: 'l',
     })
 
-    test('should accept a medium size type', () => {
-      const { button } = getJSIconButtonProps({
-        ariaLabel: 'icon button',
-        size: 'm',
-      })
+    expect(button.cssProps).toContain('width: 3rem')
+    expect(button.styles.width).toEqual('3rem')
 
-      expect(button.cssProps).toContain('width: 2.5rem')
-      expect(button.styles.width).toEqual('2.5rem')
-
-      expect(button.cssProps).toContain('padding-inline: 0')
-      expect(button.styles.paddingInline).toEqual('0')
-    })
-
-    test('should accept a large size type', () => {
-      const { button } = getJSIconButtonProps({
-        ariaLabel: 'icon button',
-        size: 'l',
-      })
-
-      expect(button.cssProps).toContain('width: 3rem')
-      expect(button.styles.width).toEqual('3rem')
-
-      expect(button.cssProps).toContain('padding-inline: 0')
-      expect(button.styles.paddingInline).toEqual('0')
-    })
+    expect(button.cssProps).toContain('padding-inline: 0')
+    expect(button.styles.paddingInline).toEqual('0')
   })
 })
